0a42d9d053ac5081768f9cbeb053b9174c2a9f24
Alex Kempton <alex@blackdiamond.co.uk>
2016-07-11 11:04:54 +0100

    crummy random positions function for lights

--- modules/lights.js
+++ modules/lights.js
@@ -3,6 +3,14 @@ var threeEnv = require('./threeEnv');
 var gui = require('./gui').addFolder('Lights');
 
 
+var params = {
+	randomPositions: function() {
+		randomPositions();
+	}
+}
+
+gui.add(params, 'randomPositions');
+
 var ambientLight = new THREE.AmbientLight( 0xffffff, 0.2 );
 
 var lights = [];
@@ -19,5 +27,14 @@ threeEnv.scene.add( lights[ 0 ] );
 threeEnv.scene.add( lights[ 1 ] );
 threeEnv.scene.add( lights[ 2 ] );
 
-
 threeEnv.scene.add( ambientLight );
+
+var randomPositions = function() {
+
+	console.log('l');
+
+	lights[ 0 ].position.set( (Math.random() * 500) - 250, (Math.random() * 500) - 250, (Math.random() * 500) - 250 );
+	lights[ 1 ].position.set( (Math.random() * 500) - 250, (Math.random() * 500) - 250, (Math.random() * 500) - 250 );
+	lights[ 2 ].position.set( (Math.random() * 500) - 250, (Math.random() * 500) - 250, (Math.random() * 500) - 250 );
+
+}
