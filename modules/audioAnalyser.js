a12aa97b727cd493297ee77d46fbaa4f1b0720d2
Alex Kempton <alex@blackdiamond.co.uk>
2016-07-12 13:58:54 +0100

    frequencies controlled by dat gui

--- modules/audioAnalyser.js
+++ modules/audioAnalyser.js
@@ -1,4 +1,5 @@
 var Freq = require('./freq');
+var gui = require('./gui').addFolder('Frequencies');
 
 var audioContext, analyser, source, stream, freqs;
 
@@ -20,21 +21,52 @@ source = audioContext.createMediaElementSource(audio);
 // Set up audio lib
 analyser = new Freq(audioContext);
 
+var ranges = {
+	'a0': 0,
+	'a1': 0.01,
+	'b0': 0.1,
+	'b1': 0.5
+}
+
+
+var a0 = gui.add(ranges, 'a0', 0, 1);
+var a1 = gui.add(ranges, 'a1', 0, 1);
+var b0 = gui.add(ranges, 'b0', 0, 1);
+var b1 = gui.add(ranges, 'b1', 0, 1);
+
+a0.onChange(function(value) {
+	stream.bands[0].updateLower(value);
+});
+
+a1.onChange(function(value) {
+	stream.bands[0].updateUpper(value);
+});
+
+b0.onChange(function(value) {
+	stream.bands[1].updateLower(value);
+});
+
+b1.onChange(function(value) {
+	stream.bands[1].updateUpper(value);
+});
+
 // Create a stream
 stream = analyser.createStream(source, {
 	bandVals: [
-		[ 0, 0.01 ],
-		[ 0.1, 0.5]
+		[ ranges['a0'], ranges['a1'] ],
+		[ ranges['b0'], ranges['b1'] ]
 	]
 });
 
 // Create a new visualiser from stream passing in an empty div
 stream.visualiser(elVisualiser);
 
+// Should only happen once per tick
 var updateLevels = function() {
 	stream.update();
 }
 
+// Can be used to get data in many modules
 var getLevels = function() {
 	return stream.read();
 }
